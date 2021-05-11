37384d10c7dc8adb22cdf62935edfc1886527631
Patrique Legault <patrique.legault@aftia.com>
2021-05-11 11:52:09 -0400

    Adding form fragments folder

---
+++ ui.apps/src/main/content/jcr_root/apps/forms-wknd/clientlibs/clientlib-forms-wknd-vacation/js/sidebar.js
@@ -0,0 +1,13 @@
+const social = "SOCIAL";
+
+/**
+ * Retrieve the details of the sidebar
+ * @name funcName sideBarProp
+ * @memberof wknd-vacation
+ * @param string som The som expression of the field. Note that the only som expressions supported are for text components
+ * @param string id The identifier of the field used to determine what text to set in the som expression
+ * @return string
+ */
+function sideBarProp(som, id) {
+    return som;
+}
