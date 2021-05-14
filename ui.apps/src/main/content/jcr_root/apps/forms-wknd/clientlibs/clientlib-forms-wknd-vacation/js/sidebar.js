8d435517ce2bd198d47cd2d2f9d958de93abbd07
Patrique Legault <patrique.legault@aftia.com>
2021-05-14 14:22:31 -0400

    WKND-22 #comment Adding client lib logic requested by client

--- ui.apps/src/main/content/jcr_root/apps/forms-wknd/clientlibs/clientlib-forms-wknd-vacation/js/sidebar.js
+++ ui.apps/src/main/content/jcr_root/apps/forms-wknd/clientlibs/clientlib-forms-wknd-vacation/js/sidebar.js
@@ -1,7 +1,6 @@
 const social = "SOCIAL";
 const type = "TYPE";
 const length = "LENGTH";
-const size = "SIZE";
 const difficulty = "DIFFICULTY";
 const price = "PRICE";
 
@@ -12,7 +11,7 @@ const price = "PRICE";
  * @param {string} id The identifier of the field used to determine what text to set in the som expression.
  */
 function sideBarProp(som, id) {
-    try {    
+    try {
         var node = guideBridge.resolveNode(som);
         if (id === social) {
             $("#" + node.id + " p").html("Activity<br/> <b>Social</b>");
@@ -20,14 +19,44 @@ function sideBarProp(som, id) {
             $("#" + node.id + " p").html("Adventure Type<br/> <b>Day Trip</b>");
         } else if (id === length) {
             $("#" + node.id + " p").html("Trip Length<br/> <b>1</b>");
-        } else if (id === size) {
-            $("#" + node.id + " p").html("Group Size<br/> <b>6 - 8</b>");
         } else if (id === difficulty) {
             $("#" + node.id + " p").html("Difficulty<br/> <b>Beginner</b>");
         } else if (id === price) {
-            $("#" + node.id + " p").html("Price<br/> <b>100$ USD</b>");
+            $("#" + node.id + " p").html("Price<br/> <b>50$ USD</b>");
         } else {
             console.log("Failed to identify the correct id for the given operation");
+        }
+    }
+    catch (e) {
+        console.error("Failed to retrieve the som or the id is undefined for the sidebar operation");
+    }
+}
+
+/**
+ * Retrieve the price per user for the group outing
+ * @name updatePricing Returns the price for the group
+ * @param {string} som The som expression of the field. Note that the only som expressions supported are for text components
+ * @param {number} groupSize The size of the group
+ */
+function updatePricing(som, groupSize) {
+    try {
+        var node = guideBridge.resolveNode(som);
+        if (groupSize) {
+            var cost = 0;
+            var costStr = undefined;
+            if (groupSize == 1) {
+                $("#" + node.id + " p").html("Price<br/> <b>50$ USD</b>");
+            } else if (groupSize > 1 && groupSize < 6) {
+                cost = 45 * groupSize;
+                costStr = "Price<br/> <b>" + cost + "$ USD</b>";
+                $("#" + node.id + " p").html(costStr);
+            } else if (groupSize > 5) {
+                cost = 40 * groupSize;
+                costStr = "Price<br/> <b>" + cost + "$ USD</b>";
+                $("#" + node.id + " p").html(costStr);
+            } else {
+                $("#" + node.id + " p").html("<b>Please enter a group size between 1 and 15</b>");
+            }
         }
     }
     catch (e) {
